57892a17d89640f7e21760c3dcd19d694d8091fe
stevewooo <461437874@qq.com>
2019-05-04 20:52:12 +0800

    a

--- server/models/mysql/defineModel.js
+++ server/models/mysql/defineModel.js
@@ -19,6 +19,72 @@ async function defineModel(swc){
 		create_at : {type : Sequelize.STRING()},
 		update_at : {type : Sequelize.STRING()},
 	})
+
+	/***************************************************************************
+	** 表情素材部分
+	*
+	*/
+	/**
+	* 表情原图表，也就是对应每个表情包库的item
+	*/
+	// swc.dao.models.origin_stickers = swc.dao.seq.define("origin_stickers", {
+	// 	origin_sticker_id : {type : Sequelize.STRING(32)},
+	// 	sticker_category_id : {type : Sequelize.STRING(32)}, //所属分类
+	// 	default_info : {type : Sequelize.TEXT}, //图片在这里面，info类必须都是个json数组。图片是在解析时才用到，其他时候无关
+
+	// 	create_by : {type : Sequelize.STRING(32)},
+	// 	update_by : {type : Sequelize.STRING(32)},
+	// 	create_at : {type : Sequelize.STRING()},
+	// 	update_at : {type : Sequelize.STRING()},
+	// })
+
+	// /**
+	// * 表情库分类
+	// */
+	// swc.dao.models.sticker_categories = swc.dao.seq.define("sticker_categories", {
+	// 	sticker_category_id : {type : Sequelize.STRING(32)},
+	// 	title : {type : Sequelize.TEXT},
+	// 	description : {type : Sequelize.TEXT},
+	// 	icon_url : {type : Sequelize.TEXT}, //原图分类的图标url
+
+	// 	create_by : {type : Sequelize.STRING(32)},
+	// 	update_by : {type : Sequelize.STRING(32)},
+	// 	create_at : {type : Sequelize.STRING()},
+	// 	update_at : {type : Sequelize.STRING()},
+	// })
+
+	// *
+	// * 具体用户创建的表情包。
+	
+	// swc.dao.models.stickers = swc.dao.seq.define("stickers", {
+	// 	sticker_id : {type : Sequelize.STRING(32)},
+	// 	info : {type : Sequelize.TEXT}, //表情包的内容。必须是个json数组。默认拿0游标位
+
+	// 	create_by : {type : Sequelize.STRING(32)},
+	// 	update_by : {type : Sequelize.STRING(32)},
+	// 	create_at : {type : Sequelize.STRING()},
+	// 	update_at : {type : Sequelize.STRING()},
+	// })
+
+	// /***************************************************************************
+	// ** 内容部分
+	// *
+	// */
+	// /**
+	// * 主题分类
+	// */
+	// swc.dao.models.theme_categories = swc.dao.seq.define("theme_categories", {
+	// 	theme_category_id : {type : Sequelize.STRING(32)},
+	// 	title : {type : Sequelize.TEXT},
+	// 	description : {type : Sequelize.TEXT},
+	// 	icon_url : {type : Sequelize.TEXT}, //原图分类的图标url
+
+	// 	create_by : {type : Sequelize.STRING(32)},
+	// 	update_by : {type : Sequelize.STRING(32)},
+	// 	create_at : {type : Sequelize.STRING()},
+	// 	update_at : {type : Sequelize.STRING()},
+	// })
+
 	// swc.dao.models.demos = swc.dao.seq.define("demos", {
 	// 	demo_id : {type : Sequelize.STRING(32)},
 	// 	name : {type : Sequelize.TEXT()},
